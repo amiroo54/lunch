9f7273e766ca0dfd924a1fe1de66a569002d5878
amiroo54 <amiroo.f54@gmail.com>
2025-12-29 16:09:46 +0330

    updated the port

--- app.py
+++ app.py
@@ -231,4 +231,4 @@ def lunch():
                            recent_events=recent_events)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=2000)
